af020f6bfcfd1dca591d055e62631d699af2488a
yingyi mao <yingyi_mao@cmu.ac.th>
2024-08-09 07:27:32 +0700

    lab2e

---
+++ lab2/src/services/EventService.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/Mao0871/SE331-Lab2.1',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvent() {
+    return apiClient.get('/events')
+  }
+}
---
+++ lab2/src/views/EventListView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import CategoryCard from '@/components/CategoryCard.vue'
+import Event from '@/type/Event'
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService'
+
+const events = ref<Event[]>(null)
+
+onMounted(() => {
+  EventService.getEvent()
+    .then((response) => {
+      // console.log(response.data)
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <!-- new element -->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <CategoryCard v-for="eventi in events" :key="eventi.id" :event="eventi" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
